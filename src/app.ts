99cb9750df3e8d96d6b1af41d5e56775abe3c9e7
Alisson F <alissonfelipenettt@gmail.com>
2022-05-01 10:00:16 -0300

    Arquivos de config,. database e rotas criados
    Arquivos inicias de cada um dos itens acima foram criados e adicionado alguns itens.

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,17 @@
-console.log('deu bom!')
+import express from "express";
+import config from "config";
+import router from "./Routes/Router";
+import database, { connection } from "./db/connection";
+
+const app = express();
+const port = config.get<number>("port");
+
+app.use(express.json());
+
+app.use("/api/", router);
+
+app.listen(port, async () => {
+  await connection(database);
+
+  console.log(`Server is running on port ${port}`);
+});
