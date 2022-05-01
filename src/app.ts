92c790e6ffd6ebc30b80ffbf7c21392d436b49ee
Alisson F <alissonfelipenettt@gmail.com>
2022-05-01 10:20:16 -0300

    Middleware logger requisição
    Adicionado o middleware responsavel pelo logger de requisição http

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,17 @@
 import express from "express";
 import config from "config";
+
 import router from "./Routes/Router";
 import database, { connection } from "./db/connection";
 import Logger from "../config/logger";
+import morganMiddleware from "./middleware/morganMiddleware";
 
 const app = express();
 const port = config.get<number>("port");
 
 app.use(express.json());
 
+app.use(morganMiddleware);
 app.use("/api/", router);
 
 app.listen(port, async () => {
